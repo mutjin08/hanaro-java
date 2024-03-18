879309e6c697d71b6c8aeb67cf94ac259f139e18
mutjin08 <mutjin08@gmail.com>
2024-03-19 01:13:21 +0900

    24-03-19

--- quiz/src/Quiz4.java
+++ quiz/src/Quiz4.java
@@ -18,11 +18,27 @@ public class Quiz4 {
         System.out.print("1부터 40사이의 정수 한 개를 입력하세요: ");
         int n = scanner.nextInt();
 
-        String[] answer = new String[n+1];
+        //정답
+        String comp;
+        String user;
         for (int i = 1; i <= n; i++) {
-            answer[i] = isX(i);
-        }
+            comp = isX(i);
 
+            if(i%2==1){
+                System.out.println("컴퓨터: "+comp);
+                continue;
+            }
+
+            System.out.print("나: ");
+            user = scanner.next();
+
+            if(!comp.equals(user)){
+                System.out.println("컴퓨터 승!");
+                System.out.println("게임종료");
+                return;
+            }
+        }
+        System.out.println("무승부!");
         
         scanner.close();
     }
